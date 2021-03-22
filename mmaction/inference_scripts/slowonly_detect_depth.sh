f033930248dc2372c7f940f67cc82fed363eef21
Koooko96 <742936939@qq.com>
2021-03-22 15:42:39 +0800

    updated mmaction

---
+++ mmaction/inference_scripts/slowonly_detect_depth.sh
@@ -0,0 +1,1 @@
+bash tools/dist_test_recognizer.sh configs/slowonly_addvalset_addtestsetv1_detect_lr0.01_cropratio0.2_epoch65_depth.py work_dirs_testphase/slowonly_addvalset_addtestsetv1_detect_lr0.01_cropratio0.2_epoch65_depth/epoch_60.pth 3 --out val_result --name slowonly_addvalset_addtestsetv1_detect_depth_epoch60 --color_or_depth depth
